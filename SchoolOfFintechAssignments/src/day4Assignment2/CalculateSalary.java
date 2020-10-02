9ccc2e8689b694dee611a9f1c55b994465aab086
Param Attri <attri.param@gmail.com>
2020-10-02 22:30:37 +0530

    Initial Commit

---
+++ SchoolOfFintechAssignments/src/day4Assignment2/CalculateSalary.java
@@ -0,0 +1,30 @@
+package day4Assignment2;
+import java.util.Scanner;
+public class CalculateSalary {
+	
+	public static int calculateSal(int salary, int shifts) {
+		int savings;
+		savings = (int)(salary - ((0.20 * salary) + (0.30 * salary)));
+		savings = (int)(savings + (0.02 * salary * shifts));
+		return savings;
+	}
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int salary = sc.nextInt();
+		int shifts = sc.nextInt();
+		int savings = 0;
+		
+		if(salary > 8000)
+			System.out.println("Salary is too large");
+		else if(shifts < 0)
+			System.out.println("Shifts too small");
+		else if(salary < 0)
+			System.out.println("Salary too small");
+		else {
+			savings = calculateSal(salary,shifts);
+			System.out.println(savings);
+			
+			}
+		}
+}
