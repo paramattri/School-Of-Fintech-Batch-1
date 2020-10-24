2777f1e007bf8b6a115c20db723fd6bcebfb331e
Param Attri <attri.param@gmail.com>
2020-10-24 16:52:27 +0530

    Initial commit

---
+++ CalculatorLibrarySimple/src/com/manipal/library/calculatorSimple/CalculatorSimple.java
@@ -0,0 +1,25 @@
+package com.manipal.library.calculatorSimple;
+
+public class CalculatorSimple {
+	
+	public int addition(int number1, int number2) {
+		
+		return number1 + number2;
+	}
+	
+	public int subtraction(int number1, int number2) {
+		
+		return number1 - number2;
+	}
+	
+	public int multiply(int number1, int number2) {
+		
+		return number1 * number2;
+	}
+	
+	public float division(int number1, int number2) {
+		
+		return (float)number1 / number2;
+	}
+
+}
