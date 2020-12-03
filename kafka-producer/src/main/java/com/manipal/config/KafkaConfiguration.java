52b8682c904df563f111ac740ab324f6d4bed94c
Param Attri <attri.param@gmail.com>
2020-12-04 00:36:49 +0530

    Initial commit

--- kafka-producer/src/main/java/com/manipal/config/KafkaConfiguration.java
+++ kafka-producer/src/main/java/com/manipal/config/KafkaConfiguration.java
@@ -8,7 +8,11 @@ import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import com.manipal.model.Book;
 
 @Configuration
 public class KafkaConfiguration {
@@ -24,4 +28,26 @@ public class KafkaConfiguration {
 		return producerFactory;
 		
 	}
+	
+	@Bean
+    public KafkaTemplate<String, String> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+	
+	@Bean
+	public ProducerFactory<String, Book> producerFactoryBook(){	
+		Map<String, Object> configs = new HashMap<>();
+		//config for server
+		configs.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+		configs.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+		configs.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+		ProducerFactory<String, Book> producerFactory = new DefaultKafkaProducerFactory<>(configs);
+		return producerFactory;
+		
+	}
+	
+	@Bean
+    public KafkaTemplate<String, Book> kafkaTemplateBook() {
+        return new KafkaTemplate<String, Book>(producerFactoryBook());
+    }
 }
